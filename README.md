a24f2fdc9456dca2ca20fb7be0115e14067d98fe
CrystalW11 <crystal.warmack@gmail.com>
2025-12-09 18:12:45 -0700

    updatted readme.md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Exercise---Add-suggested-donation-data
+
+![alt text](image.png)
+
+![alt text](image-1.png)
+
+![alt text](image-2.png)
+
+![alt text](image-3.png)
+
+## Output: `dotnet build` then `dotnet run`
+
+![alt text](image-4.png)
+
--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a79fe410323e0e6edb513e6f672ebcf673b4cd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c5a5e232856aa59e10d90a06deacfa28f443e87")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
